fb8df4af105697822a616494830cc28f57913ee4
Jakub.Kidawa <jakub.kidawa@u-blox.com>
2018-01-12 17:20:01 +0100

    LaNet with cropping and only center cammera, car drives quite ok until break in the line

---
+++ trainTheModel.py
@@ -0,0 +1,70 @@
+import numpy as np
+import csv
+import cv2
+import tensorflow as tf
+
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers import Lambda
+from keras.layers import Cropping2D
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+# command line flags
+flags.DEFINE_string('dataPath', '', "Patch to driving_log.csv and IMG folder")
+flags.DEFINE_string('epochs', '10', "Number of training epochs ")
+print(FLAGS.dataPath)
+lines = []
+with open(FLAGS.dataPath+'\\driving_log.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        for line in reader:
+            lines.append(line)
+ 
+images = []
+measurements = []
+for line in lines:
+    sourcePath = line[0]
+    filename = sourcePath.split('\\')[-1]
+    image = cv2.imread(FLAGS.dataPath+'\\IMG\\'+filename)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(6, 10, 10))
+model.add(MaxPooling2D((2, 2)))
+#model.add(Dropout(0.9))
+model.add(Activation('relu'))
+model.add(Convolution2D(16, 5, 5))
+model.add(MaxPooling2D((2, 2)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Activation('relu'))
+model.add(Dense(84))
+model.add(Activation('relu'))
+model.add(Dense(1))
+'''
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Flatten())
+model.add(Dense(1))
+'''
+
+
+model.compile(loss='mse', optimizer='adam')
+history = model.fit(X_train,y_train,nb_epoch=int(FLAGS.epochs), validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
+
